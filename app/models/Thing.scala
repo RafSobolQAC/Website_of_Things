64aae8ecec720e1aea570dc398986f8950c4fb92
Raf Sobol <rafal.sobol@academytrainee.com>
2020-03-17 12:24:58 +0000

    Initial commit

---
+++ app/models/Thing.scala
@@ -0,0 +1,22 @@
+package models
+
+import play.api.libs.json.OFormat
+
+case class Thing(
+                  name: String,
+                  price: BigDecimal,
+
+                  tags: List[Tag]
+                )
+
+case class Tag(
+                name: String
+              )
+
+
+object JsonFormats {
+  import play.api.libs.json.Json
+
+  implicit val feedFormat: OFormat[Tag] = Json.format[Tag]
+  implicit val userFormat: OFormat[Thing] = Json.format[Thing]
+}
